bc851b8cd79a51b80ac6d6de573e3684e7484370
Karina Koval <kkoval98@gmail.com>
2023-10-06 12:57:37 +0300

    feat: add link option to buttom component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,7 +1,15 @@
+@props(['href' => null, 'message' => null])
+
 @php
     $buttonClass = 'mt-6 w-full grid inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-sm lg:text-base text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4';
 @endphp
 
-<button class="{{ $buttonClass }}" type="{{ $type ?? 'button' }}" {{ $attributes->merge() }}>
-    {{ $slot }}
-</button>
+@if ($href)
+    <a class="{{ $buttonClass }}" href="{{ $href }}{{ $message }}" target="_blank">
+        {{ $slot }}
+    </a>
+@else
+    <button class="{{ $buttonClass }}" type="{{ $type ?? 'button' }}" {{ $attributes->merge() }}>
+        {{ $slot }}
+    </button>
+@endif
