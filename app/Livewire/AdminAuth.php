e4c9836b8076c4f02a4f509aca987d60ee199160
Karina Koval <kkoval98@gmail.com>
2023-10-06 13:31:05 +0300

    feat: create AdminAuthForm

--- app/Livewire/AdminAuth.php
+++ app/Livewire/AdminAuth.php
@@ -3,13 +3,28 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Features\SupportRedirects\Redirector;
+use Illuminate\View\View;
+use App\Livewire\Forms\AdminAuthForm;
 
 class AdminAuth extends Component
 {
-    public $login = true;
+    public $admin = false;
 
-    public function render()
+    public AdminAuthForm $form;
+
+    public function render(): View
     {
         return view('livewire.admin-auth');
+    }
+
+    public function login(): Redirector
+    {
+        return redirect('/list');
+    }
+
+    public function logout(): Redirector
+    {
+        return redirect('/register');
     }
 }
