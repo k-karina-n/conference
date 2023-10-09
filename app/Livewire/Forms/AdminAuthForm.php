bbc4f63b631e87764e4e211bafbe66e31d7dba4a
Karina Koval <kkoval98@gmail.com>
2023-10-09 18:46:16 +0300

    docs(AdminAuthForm.php): add comments

--- app/Livewire/Forms/AdminAuthForm.php
+++ app/Livewire/Forms/AdminAuthForm.php
@@ -7,9 +7,19 @@ use Livewire\Form;
 
 class AdminAuthForm extends Form
 {
+    /**
+     * Admin email to be validated.
+     *
+     * @var string
+     */
     #[Rule('required|email|exists:admins,email', attribute: 'email')]
     public string $email;
 
+    /**
+     * Admin password to be validated.
+     *
+     * @var string
+     */
     #[Rule('required', attribute: 'password')]
     public string $password;
 }
