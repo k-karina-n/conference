17ed9ddcd85510c5f631c7aaf6187b17afe81cb7
Karina Koval <kkoval98@gmail.com>
2023-10-10 12:49:55 +0300

    feat: update input style

--- resources/views/components/form-user-data.blade.php
+++ resources/views/components/form-user-data.blade.php
@@ -1,7 +1,7 @@
-<div wire:init="getCookie" class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:gap-6">
+<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:gap-6" wire:init="getCookie">
     <x-input label="first name" name="first_name" />
     <x-input label="last name" name="last_name" />
     <x-input label="phone" name="phone" type="phone" x-mask="+99 (999) 999-9999" />
     <x-input label="email" name="email" type="email" />
-    <x-input label="country" name="country" tag="select" />
 </div>
+<x-input label="country" name="country" tag="select" />
