ed6101c8dc42333f5e0eafc22029c36a4517f51f
Karina Koval <kkoval98@gmail.com>
2023-10-10 16:26:52 +0300

    feat(UserRegistration.php): add UserObject.php

--- app/Livewire/UserRegistration.php
+++ app/Livewire/UserRegistration.php
@@ -2,27 +2,13 @@
 
 namespace App\Livewire;
 
-use Livewire\Component;
-use Livewire\WithFileUploads;
-use App\Livewire\Forms\UserDataForm;
+use App\Livewire\Objects\UserObject;
 use App\Models\User;
 use Livewire\Features\SupportRedirects\Redirector;
 use Illuminate\View\View;
 
-class UserRegistration extends Component
+class UserRegistration extends UserObject
 {
-    /**
-     * Trait for handling file uploads in the component.
-     */
-    use WithFileUploads;
-
-    /**
-     * The form to validate user data.
-     *
-     * @var UserDataForm The form object.
-     */
-    public UserDataForm $form;
-
     /**
      * Whether the first step of the registration process is visible.
      *
@@ -62,27 +48,6 @@ class UserRegistration extends Component
     public function render(): View
     {
         return view('livewire.registration-form');
-    }
-
-    /**
-     * Call method from UserDataForm to get user data from session.
-     * 
-     * @return void
-     */
-    public function getSessionData(): void
-    {
-        $this->form->getSessionData();
-    }
-
-    /**
-     * Call method from UserDataForm to save user data to session.
-     * 
-     * @param string $name Input name
-     * @return void
-     */
-    public function updateSessionData(string $name): void
-    {
-        $this->form->updateSessionData($name);
     }
 
     /**
